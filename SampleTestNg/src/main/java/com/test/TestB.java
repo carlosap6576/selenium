3b74253f7cae3ef69f54d9a66567c14133e932c0
carlosap6576 <carlosap6576@gmail.com>
2020-10-27 22:16:44 -0400

    Add notes

--- SampleTestNg/src/main/java/com/test/TestB.java
+++ SampleTestNg/src/main/java/com/test/TestB.java
@@ -33,3 +33,33 @@ public class TestB {
         }
     }
 }
+
+//C:\Users\cperez\gocode\src\github.com\selenium\SampleTestNg>java -cp C:\Users\cperez\gocode\src\github.com\selenium\SampleTestNg\lib\*;C:\Users\cperez\gocode\src\github.com\selenium\SampleTestNg\bin org.testng.TestNG testng.xml
+//        ...
+//        ... TestNG 6.14.3 by Cédric Beust (cedric@beust.com)
+//        ...
+//
+//        *******************
+//        launching chrome browser
+//        Starting ChromeDriver 86.0.4240.22 (398b0743353ff36fb1b82468f63a3a93b4e2e89e-refs/branch-heads/4240@{#378}) on port 44262
+//        Only local connections are allowed.
+//        Please see https://chromedriver.chromium.org/security-considerations for suggestions on keeping ChromeDriver safe.
+//        ChromeDriver was started successfully.
+//        Oct 27, 2020 10:08:43 PM org.openqa.selenium.remote.ProtocolHandshake createSession
+//        INFO: Detected dialect: W3C
+//        Closing chrome browser
+//        PASSED: testGooglePageTitleInChrome
+//
+//        ===============================================
+//        TestNG Test Group
+//        Tests run: 1, Failures: 0, Skips: 0
+//        ===============================================
+//
+//
+//        ===============================================
+//        Main Test Suite
+//        Total tests run: 1, Failures: 0, Skips: 0
+//        ===============================================
+//
+//
+//        C:\Users\cperez\gocode\src\github.com\selenium\SampleTestNg>
